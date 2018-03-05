2a8eb35a0a724db96653df6f30fb4bc225f3dbed
Andrew Bares <anbare@microsoft.com>
2018-03-05 10:18:27 -0800

    Add comments

--- CPP/DesktopToastsCppApp/DesktopNotificationManagerCompat.h
+++ CPP/DesktopToastsCppApp/DesktopNotificationManagerCompat.h
@@ -24,11 +24,39 @@ class DesktopNotificationHistoryCompat;
 class DesktopNotificationManagerCompat
 {
 public:
+
+    /// <summary>
+    /// If not running under the Desktop Bridge, you must call this method to register your AUMID with the Compat library and to
+    /// register your COM CLSID and EXE in LocalServer32 registry. Feel free to call this regardless, and we will no-op if running
+    /// under Desktop Bridge. Call this upon application startup, before calling any other APIs.
+    /// </summary>
+    /// <param name="aumid">An AUMID that uniquely identifies your application.</param>
+    /// <param name="clsid">The CLSID of your NotificationActivator class.</param>
     static HRESULT RegisterAumidAndComServer(const wchar_t *aumid, GUID clsid);
+
+    /// <summary>
+    /// Registers your module to handle COM activations. Call this upon application startup.
+    /// </summary>
     static HRESULT RegisterActivator();
+
+    /// <summary>
+    /// Creates a toast notifier. You must have called RegisterActivator first (and also RegisterAumidAndComServer if you're a classic Win32 app), or this will throw an exception.
+    /// </summary>
     static HRESULT CreateToastNotifier(IToastNotifier **notifier);
+
+    /// <summary>
+    /// Creates a toast notification. This is simply a convenience helper method.
+    /// </summary>
     static HRESULT CreateToastNotification(ABI::Windows::Data::Xml::Dom::IXmlDocument* content, IToastNotification** notification);
+
+    /// <summary>
+    /// Gets the DesktopNotificationHistoryCompat object. You must have called RegisterActivator first (and also RegisterAumidAndComServer if you're a classic Win32 app), or this will throw an exception.
+    /// </summary>
     static HRESULT get_History(DesktopNotificationHistoryCompat *history);
+
+    /// <summary>
+    /// Gets a boolean representing whether http images can be used within toasts. This is true if running under Desktop Bridge.
+    /// </summary>
     static bool CanUseHttpImages();
 
 private:
@@ -46,14 +74,46 @@ private:
 class DesktopNotificationHistoryCompat
 {
 public:
+
+    /// <summary>
+    /// Removes all notifications sent by this app from action center.
+    /// </summary>
     HRESULT Clear();
+
+    /// <summary>
+    /// Gets all notifications sent by this app that are currently still in Action Center.
+    /// </summary>
     HRESULT GetHistory(__FIVectorView_1_Windows__CUI__CNotifications__CToastNotification **toasts);
+
+    /// <summary>
+    /// Removes an individual toast, with the specified tag label, from action center.
+    /// </summary>
+    /// <param name="tag">The tag label of the toast notification to be removed.</param>
     HRESULT Remove(const wchar_t *tag);
+
+    /// <summary>
+    /// Removes a toast notification from the action using the notification's tag and group labels.
+    /// </summary>
+    /// <param name="tag">The tag label of the toast notification to be removed.</param>
+    /// <param name="group">The group label of the toast notification to be removed.</param>
     HRESULT Remove(const wchar_t *tag, const wchar_t *group);
+
+    /// <summary>
+    /// Removes a group of toast notifications, identified by the specified group label, from action center.
+    /// </summary>
+    /// <param name="group">The group label of the toast notifications to be removed.</param>
     HRESULT RemoveGroup(const wchar_t *group);
+
+    /// <summary>
+    /// Do not call this. Instead, call DesktopNotificationManagerCompat.get_History() to obtain an instance.
+    /// </summary>
     DesktopNotificationHistoryCompat(const wchar_t *aumid, Microsoft::WRL::ComPtr<IToastNotificationHistory> history);
 
-    // TODO: This constructor should be removed? Can we somehow make this support ComPtr and stuff?
+    // TODO: The default constructor should be removed? Can we somehow make this support ComPtr and stuff?
+
+    /// <summary>
+    /// Do not call this. Instead, call DesktopNotificationManagerCompat.get_History() to obtain an instance.
+    /// </summary>
     DesktopNotificationHistoryCompat();
 
 private:
