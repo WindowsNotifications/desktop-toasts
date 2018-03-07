149b35f003b2d960420222d8d009ff554b79fd67
Andrew Bares <anbare@microsoft.com>
2018-03-07 12:40:11 -0800

    Includes updates

--- CPP/DesktopToastsCppApp/DesktopToastsSample.cpp
+++ CPP/DesktopToastsCppApp/DesktopToastsSample.cpp
@@ -1,8 +1,7 @@
-#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 #include <atlstr.h>
 #include "DesktopNotificationManagerCompat.h"
 #include "NotificationActivationCallback.h"
+#include <SDKDDKVer.h>
 #include <string>
 #include <windows.ui.notifications.h>
 #include <wrl.h>
